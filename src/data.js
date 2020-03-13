4cdf911947b2d3139eff8247fbfe74d4d0ebdd3e
rinconcitog <60704930+rinconcitog@users.noreply.github.com>
2020-03-13 16:38:04 -0600

    cambios a la carpeta src

--- src/data.js
+++ src/data.js
@@ -1,13 +1,16 @@
-// import data from './data/injuries/injuries.js';
-// import data from './data/lol/lol.js';
-// import data from './data/patient/patient.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/steam/steam.js';
-// import data from './data/steam/worldbank.js';
 
-// esta es una función de ejemplo
 
-export const example = () => {
-  return 'example';
+
+window.rnmgo = {
+  filterData(data, condition)  {
+    let tiposDeGenero;
+    if (typeof condition === "string")
+    {
+      tiposDeGenero = data.filter(generos =>{
+        return generos.gender.indexOf(condition) !== -1;
+       })
+
+    }
+  return tiposDeGenero;
+}
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,64 @@
-import { example } from './data.js';
+insertcards//llamar la data con fetch
+let rinckandmorty;
 
-console.log(example);
 
+fetch('data/rickandmorty/rickandmorty.json')
+.then(answer =>answer.json())
+.then(answer =>{
+  rinckandmorty = answer;
+  imageRickandMorty(rinckandmorty);
+} )
+
+
+function imageRickandMorty (data){
+  for (let i = 0; i < data.results.length ;i++) {
+    document.getElementById("insertcards").innerHTML += `
+    <div id="card">
+              <div class="card-image" style="background-image: url(https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/${data.results[i].id}.jpeg);">
+              </div>
+              <div class="card-stacked">
+                <div class="card-content">
+                <h3> ${data.results[i].name}</h3>
+                <p>Status: ${data.results[i].status}</p>
+                <p>Specie: ${data.results[i].species}</p>
+                <p>Gender: ${data.results[i].gender}</p>
+                </p>
+                </div>
+              </div>
+    </div>`
+}
+}
+document.getElementById('link_todos').addEventListener('click', () => {
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('todos').style.display = 'block';
+});
+
+
+//filtrar por genero
+
+
+document.getElementById("genero_selector").addEventListener("change",() =>{
+    document.getElementById("insertcards").innerHTML = " "
+
+     let generos =document.getElementById("genero_selector").value
+      for (let i = 0; i <window.rnmgo.filterData(rinckandmorty.results, generos).length ; i++){
+
+       document.getElementById("insertcards").innerHTML += `
+       <div id="card">
+                 <div class="card-image" style="background-image: url(https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/${window.rnmgo.filterData(rinckandmorty.results, generos)[i].id}.jpeg);">
+                 </div>
+                 <div class="card-stacked">
+                   <div class="card-content">
+                   <h3> ${window.rnmgo.filterData(rinckandmorty.results, generos)[i].name}</h3>
+                   <p>Status: ${window.rnmgo.filterData(rinckandmorty.results, generos)[i].status}</p>
+                   <p>Specie: ${window.rnmgo.filterData(rinckandmorty.results, generos)[i].species}</p>
+                   <p>Gender: ${window.rnmgo.filterData(rinckandmorty.results, generos)[i].gender}</p>
+                   </p>
+                   </div>
+                 </div>
+       </div>
+
+     `;
+
+   }
+   });
