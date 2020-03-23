cce773ab0608a56e0399725cb8405b2944bb473c
rinconcitog <60704930+rinconcitog@users.noreply.github.com>
2020-03-23 10:45:33 -0600

    ultimos cambios

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 
-
+//filtrar por generos
 
 window.rnmgo = {
   filterData(data, condition)  {
@@ -12,5 +12,37 @@ window.rnmgo = {
 
     }
   return tiposDeGenero;
+}
+};
+
+//filtrar por Especie
+
+window.rnmgo2 = {
+  filterData(data, condition)  {
+    let tiposDeEspecie;
+    if (typeof condition === "string")
+    {
+      tiposDeEspecie = data.filter(especie =>{
+        return especie.species.indexOf(condition) !== -1;
+       })
+
+    }
+  return tiposDeEspecie;
+}
+};
+
+//filtrar por Estado
+
+window.rnmgo3 = {
+  filterData(data, condition)  {
+    let tiposDeEstado;
+    if (typeof condition === "string")
+    {
+      tiposDeEstado = data.filter(estado =>{
+        return estado.status.indexOf(condition) !== -1;
+       })
+
+    }
+  return tiposDeEstado;
 }
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-insertcards//llamar la data con fetch
+//llamar la data con fetch
 let rinckandmorty;
 
 
@@ -33,8 +33,12 @@ document.getElementById('link_todos').addEventListener('click', () => {
   document.getElementById('todos').style.display = 'block';
 });
 
+document.getElementById('regresar').addEventListener('click', () => {
+  document.getElementById('home').style.display = 'block';
+  document.getElementById('todos').style.display = 'none';
+});
 
-//filtrar por genero
+// filtrado por genero
 
 
 document.getElementById("genero_selector").addEventListener("change",() =>{
@@ -62,3 +66,59 @@ document.getElementById("genero_selector").addEventListener("change",() =>{
 
    }
    });
+
+   //filtrado por Especie
+
+   document.getElementById("specie_selector").addEventListener("change",() =>{
+       document.getElementById("insertcards").innerHTML = " "
+
+        let especie =document.getElementById("specie_selector").value
+         for (let i = 0; i <window.rnmgo2.filterData(rinckandmorty.results, especie).length ; i++){
+
+          document.getElementById("insertcards").innerHTML += `
+          <div id="card">
+                    <div class="card-image" style="background-image: url(https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/${window.rnmgo2.filterData(rinckandmorty.results, especie)[i].id}.jpeg);">
+                    </div>
+                    <div class="card-stacked">
+                      <div class="card-content">
+                      <h3> ${window.rnmgo2.filterData(rinckandmorty.results, especie)[i].name}</h3>
+                      <p>Status: ${window.rnmgo2.filterData(rinckandmorty.results, especie)[i].status}</p>
+                      <p>Specie: ${window.rnmgo2.filterData(rinckandmorty.results, especie)[i].species}</p>
+                      <p>Gender: ${window.rnmgo2.filterData(rinckandmorty.results, especie)[i].gender}</p>
+                      </p>
+                      </div>
+                    </div>
+          </div>
+
+        `;
+
+      }
+      });
+
+      //filtrado por Estado
+
+      document.getElementById("status_selector").addEventListener("change",() =>{
+          document.getElementById("insertcards").innerHTML = " "
+
+           let estado =document.getElementById("status_selector").value
+            for (let i = 0; i <window.rnmgo3.filterData(rinckandmorty.results, estado).length ; i++){
+
+             document.getElementById("insertcards").innerHTML += `
+             <div id="card">
+                       <div class="card-image" style="background-image: url(https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/${window.rnmgo3.filterData(rinckandmorty.results, estado)[i].id}.jpeg);">
+                       </div>
+                       <div class="card-stacked">
+                         <div class="card-content">
+                         <h3> ${window.rnmgo3.filterData(rinckandmorty.results, estado)[i].name}</h3>
+                         <p>Status: ${window.rnmgo3.filterData(rinckandmorty.results, estado)[i].status}</p>
+                         <p>Specie: ${window.rnmgo3.filterData(rinckandmorty.results, estado)[i].species}</p>
+                         <p>Gender: ${window.rnmgo3.filterData(rinckandmorty.results, estado)[i].gender}</p>
+                         </p>
+                         </div>
+                       </div>
+             </div>
+
+           `;
+
+         }
+         });
